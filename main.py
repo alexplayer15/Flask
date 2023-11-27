af0451395ca1b7d5399e1bf7c438a48405d7067b
Tom Fletcher <thomasfletcher@outlook.com>
2023-11-27 19:45:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from PIL import Image
+# from PIL import Image
 # import numpy as np
 # from keras.preprocessing.image import load_img, img_to_array
 # from keras.applications.vgg16 import preprocess_input, decode_predictions
@@ -18,29 +18,25 @@ from PIL import Image
 
 app = Flask('img_classifier')
 # Load the VGG16 model with pre-trained weights
-# model = VGG16(weights='imagenet') 
+# model = VGG16(weights='imagenet')
 
 @app.route('/')
 def hello_world():
   return render_template('index.html')
-  #return '<h1>Hello World</h1>'
 
 @app.route('/', methods=['POST'])
 def predict():
     imagefile = request.files['imagefile']
     # ------- saving the image somewhere locally -----
     # save the input image now
-    image_path = 'images/' + imagefile.filename 
+    image_path = 'images/' + imagefile.filename
     print('image path', image_path)
     imagefile.save(image_path)
-    
     #   img = load_img(image_path, target_size=(224, 224))  # Resize image to VGG16 input shape
     #   img_array = img_to_array(img)
     #   print('img shape', img_array.shape) # (224, 224, 3)
-    
     #   img_array = np.expand_dims(img_array, axis=0)  # Add a batch dimension (1, 224, 224, 3)
     #   img_array = preprocess_input(img_array)
-
     #   # Make predictions using the loaded model
     #   predictions = model.predict(img_array)
     #   decoded_predictions = decode_predictions(predictions, top=3)[0]  # Get top 3 predictions
