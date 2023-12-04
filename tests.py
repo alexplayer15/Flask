373818864ddd7310e8cbf81b450f0674d1612ce8
Alexander Player <140439449+alexplayer15@users.noreply.github.com>
2023-12-04 19:57:25 +0000

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,16 @@
+def check_health_status(website_url):
 
+    try:
+        response = requests.get(website_url)
+    
+    except requests.ConnectionError: 
+        print('Could not connect to website. It might be down.')
+
+    else:
+        if response.status_code == 200:
+            print(f'The website is healthy, it has returned a status code of {response.status_code}')
+
+        else:
+            print(f'The website might be down, it has returned a status code of {response.status_code}')
+
+check_health_status('http://localhost:5000')
